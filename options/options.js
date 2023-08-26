d437d5a15cf2f05b0f11630f657bce7759b88ee9
Crowdbotics <team@crowdbotics.com>
2023-08-26 04:09:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "forever-runner-43078",
+  url: "https://forever-runner-43078.botics.co",
+  api: "https://forever-runner-43078.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
